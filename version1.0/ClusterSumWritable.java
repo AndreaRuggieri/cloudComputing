2c9ad1f1d087ac84d96ff1316524b8295af780d7
AndreaRuggieri <106307169+AndreaRuggieri@users.noreply.github.com>
2023-05-24 23:26:13 +0200

    Create ClusterSumWritable.java

---
+++ version1.0/ClusterSumWritable.java
@@ -0,0 +1,43 @@
+package tmp;
+import org.apache.hadoop.io.Writable;
+
+public class ClusterSumWritable implements Writable {
+    private double[] sumCoordinates;
+    private int count;
+
+    public ClusterSumWritable() {
+        this(new double[0], 0);
+    }
+
+    public ClusterSumWritable(double[] sumCoordinates, int count) {
+        this.sumCoordinates = sumCoordinates;
+        this.count = count;
+    }
+
+    public double[] getSumCoordinates() {
+        return sumCoordinates;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(sumCoordinates.length);
+        for (double sumCoordinate : sumCoordinates) {
+            out.writeDouble(sumCoordinate);
+        }
+        out.writeInt(count);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        int length = in.readInt();
+        sumCoordinates = new double[length];
+        for (int i = 0; i < length; i++) {
+            sumCoordinates[i] = in.readDouble();
+        }
+        count = in.readInt();
+    }
+}
