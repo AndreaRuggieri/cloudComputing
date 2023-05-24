c0cb3a41b7d9beeb3b30d644c165e61d1f2bf595
AndreaRuggieri <106307169+AndreaRuggieri@users.noreply.github.com>
2023-05-24 23:26:43 +0200

    Create KMeansMapReduce.java

---
+++ version1.0/KMeansMapReduce.java
@@ -0,0 +1,134 @@
+package tmp;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.IntWritable;
+
+public class KMeansMapReduce
+{
+	
+	static int k;
+	static int d;
+
+	public class KMeansMapper extends Mapper<LongWritable, Text, IntWritable, PointWritable> 
+	{
+	    private PointWritable[] centroids;
+	    private final Text reducerKey = new Text();
+	    private final PointWritable reducerValue = new PointWritable();
+
+	    @Override
+	    protected void setup(Context context) throws IOException, InterruptedException 
+	    {
+	    	// Genera centroidi naive method
+	    	centroids = PointWritable.generateCentroids(k, d);
+	    }
+
+	    @Override
+	    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
+	    {
+	        // Convert the input text to a PointWritable
+	        PointWritable point = textToPoint(value);
+	        
+	        // Find the nearest centroid to the point
+	        int nearestCentroidId = getNearestCentroid(point);
+
+	        // Write the centroid id and the point to the context
+	        context.write(new IntWritable(nearestCentroidId), point);
+	    }
+
+	    private PointWritable textToPoint(Text text) 
+	    {
+	        // TODO: Convert the input text to a PointWritable
+	    	String line = text.toString();
+            if (line == null || line.length() == 0) return null;
+
+            String[] tokens = line.trim().split(",");
+
+            if (tokens.length != d) 
+            { 
+                double[] val = new double[d];
+                for(int i=1;i<=d;i++)
+                {
+                	double att = Double.parseDouble(tokens[i]);
+                	val[i] = att;
+                }
+                // reducerKey.set(Integer.parseInt(tokens[0]);
+                reducerKey.set(tokens[0]);
+                reducerValue.set(reducerValue, val);
+                context.write(reducerKey, reducerValue);
+            }
+	    	
+	    	return null;
+	    }
+
+	    private int getNearestCentroid(PointWritable point) 
+	    {
+	        // TODO: Find the nearest centroid to the point
+	        return -1;
+	    }
+	}
+	
+	public class KMeansCombiner extends Reducer<IntWritable, PointWritable, IntWritable, ClusterSumWritable> {
+	    @Override
+	    protected void reduce(IntWritable key, Iterable<PointWritable> values, Context context) throws IOException, InterruptedException {
+	        // Calculate the sum of points and the count
+	        ClusterSumWritable clusterSum = calculateClusterSum(values);
+
+	        // Write the cluster id and the cluster sum to the context
+	        context.write(key, clusterSum);
+	    }
+
+	    private ClusterSumWritable calculateClusterSum(Iterable<PointWritable> values) {
+	        // TODO: Calculate the sum of points and the count
+	        return null;
+	    }
+	}
+	
+	public class KMeansReducer extends Reducer<IntWritable, PointWritable, IntWritable, PointWritable> {
+	    @Override
+	    protected void reduce(IntWritable key, Iterable<PointWritable> values, Context context) throws IOException, InterruptedException {
+	        // Calculate the sum of points and the count
+	        ClusterSumWritable clusterSum = calculateClusterSum(values);
+
+	        // Calculate the new centroid
+	        PointWritable newCentroid = calculateNewCentroid(clusterSum);
+
+	        // Write the cluster id and the new centroid to the context
+	        context.write(key, newCentroid);
+	    }
+
+	    private ClusterSumWritable calculateClusterSum(Iterable<PointWritable> values) {
+	        // TODO: Calculate the sum of points and the count
+	        return null;
+	    }
+
+	    private PointWritable calculateNewCentroid(ClusterSumWritable clusterSum) {
+	        // TODO: Calculate the new centroid
+	        return null;
+	    }
+	}
+	
+	 public static void main(final String[] args) throws Exception 
+	 {
+	        final Configuration conf = new Configuration();
+	        final Job job = new Job(conf, "kmeans");
+	        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+	        job.setJarByClass(KMeansMapReduce.class);
+
+	        job.setOutputKeyClass(Text.class);
+	        job.setOutputValueClass(IntWritable.class);
+
+	        job.setMapperClass(KMeansMapper.class);
+	        job.setReducerClass(KMeansReducer.class);
+	        
+	        k = Integer.parseInt(otherArgs[1]);
+	        d = Integer.parseInt(otherArgs[2]);
+	        
+	        FileInputFormat.addInputPath(job, new Path(args[0]));
+	        FileOutputFormat.setOutputPath(job, new Path(args[3]));
+
+	        System.exit(job.waitForCompletion(true) ? 0 : 1);
+	    }
+	
+}
