9dab478f825ae2d85eec8ade9a4b91f7af7250b1
CristianoCorsi <c.corsi99@gmail.com>
2023-05-25 20:09:18 +0200

    .

---
+++ generatoreRecord.py
@@ -0,0 +1,11 @@
+import random
+
+def generate_file(n, d, filename="input.txt"):
+    with open(filename, "w") as file:
+        for i in range(1, n + 1):
+            file.write(str(0))
+            for _ in range(d):
+                file.write(", " + str(random.random()*100))
+            file.write("\n")
+
+generate_file(200, 3)
