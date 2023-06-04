95fd329789fdde811b4a7e3fdde1cfe47ab38d6a
CristianoCorsi <c.corsi99@gmail.com>
2023-06-04 18:13:52 +0200

    Passaggio parametri

--- generatoreRecord.py
+++ generatoreRecord.py
@@ -1,12 +1,24 @@
 import random
+import argparse
+from sklearn.datasets import make_blobs
 
-def generate_file(n, d, filename="input.txt"):
+def generate_file(n, d):
+    filename = f"input_{n}_{d}.txt"
+
     with open(filename, "w") as file:
         for i in range(1, n + 1):
-            #file.write(str(0))
-            file.write(str(random.random()*100))
+            file.write(str(random.random() * 1000))
             for _ in range(d - 1):
-                file.write("," + str(random.random()*100))
+                file.write("," + str(random.random() * 1000))
             file.write("\n")
 
-generate_file(300, 3)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--num_rows", type=int,
+                        default=1000, help="Number of rows")
+    parser.add_argument("-d", "--num_columns", type=int,
+                        default=2, help="Number of columns")
+    args = parser.parse_args()
+
+    generate_file(args.num_rows, args.num_columns)
