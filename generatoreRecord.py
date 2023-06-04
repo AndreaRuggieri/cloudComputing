9067dd8a048b3ed49ffbdfb9d27f9be5dd74e649
CristianoCorsi <c.corsi99@gmail.com>
2023-06-04 18:17:15 +0200

    Modifica generazione punti -> blob

--- generatoreRecord.py
+++ generatoreRecord.py
@@ -3,13 +3,19 @@ import argparse
 from sklearn.datasets import make_blobs
 
 def generate_file(n, d):
+    centers = 5
+
+    points, y = make_blobs(n_samples=n, centers=centers, n_features=d, shuffle=True, center_box=(0, 1000), cluster_std=30)
+
     filename = f"input_{n}_{d}.txt"
 
     with open(filename, "w") as file:
-        for i in range(1, n + 1):
-            file.write(str(random.random() * 1000))
-            for _ in range(d - 1):
-                file.write("," + str(random.random() * 1000))
+        for point in points:
+            for value in range(d):
+                if value == (d - 1):
+                    file.write(str(round(point[value], 4)))
+                else:
+                    file.write(str(round(point[value], 4)) + ",")
             file.write("\n")
 
 
@@ -19,6 +25,8 @@ if __name__ == "__main__":
                         default=1000, help="Number of rows")
     parser.add_argument("-d", "--num_columns", type=int,
                         default=2, help="Number of columns")
+    parser.add_argument("-c", "--num_centers", type=int,
+                        default=3, help="Number of centers")
     args = parser.parse_args()
 
     generate_file(args.num_rows, args.num_columns)
